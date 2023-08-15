bfeffff30a12d4b5aa30e5984ee69631435cde18
szplkflrn <119355053+szplkflrn@users.noreply.github.com>
2023-08-15 10:44:54 +0200

    start server buildup

--- src/main/java/com/codecool/stackoverflowtw/controller/QuestionController.java
+++ src/main/java/com/codecool/stackoverflowtw/controller/QuestionController.java
@@ -25,7 +25,7 @@ public class QuestionController {
 
     @GetMapping("/{id}")
     public QuestionDTO getQuestionById(@PathVariable int id) {
-        return null;
+        return questionService.getQuestionById(id);
     }
 
     @PostMapping("/")
--- src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
+++ src/main/java/com/codecool/stackoverflowtw/dao/QuestionsDaoJdbc.java
@@ -1,8 +1,28 @@
 package com.codecool.stackoverflowtw.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class QuestionsDaoJdbc implements QuestionsDAO {
+
     @Override
     public void sayHi() {
         System.out.println("Hi DAO!");
     }
+
+    @Override
+    public Connection getConnection() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:src/main/resources/StackoverflowDB.db";
+            conn = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+
 }
